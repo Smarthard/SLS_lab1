06dfe14f389e7f79ccd591f507baa96718ffc1eb
Smarthard <th3smartchan@gmail.com>
2018-09-13 23:07:19 +0300

    bash-mode removed & touch works well

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 p_help() {
-	eval "echo 'h|?|help)	this text
-1|pwd) 		print working directory
+	eval "echo '1|pwd) 		print working directory
 2|cd)		change working directory
 3|ls)		list files
 4|touch)	create files
@@ -13,16 +12,11 @@ p_help() {
 menu_off=false
 bash_like=false
 
-while getopts "mb" opt; do
-	case $opt in
-		m)
-			menu_off=true;;
-		b)	
-			bash_like=true;;
-	esac
-done
-
 while : ; do
+
+	p_help
+	
+	read command
 
 	case $command in
 		1|pwd)
@@ -37,27 +31,15 @@ while : ; do
 			ls -a
 			;;
 		4|touch)
-			read files
-			touch $files
+			read -r files
+			eval "touch $files"
 			;;
 		5|rm)
 			read files
-			rm $files
+			eval "rm $files"
 			;;
 		6|exit)
 			break;;
-		h|?|help)
-			p_help
-			;;
 	esac
 	
-	if [ ! $menu_off ] && [ ! $bash_like ];
-	then
-		p_help
-	elif [ $bash_like ];
-	then
-		echo -n "$(pwd)$ "
-	fi
-
-	read command
 done
