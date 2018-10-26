b0e8ad0508e0e2d2f9228b88aad6b0238e7a2396
Smarthard <th3smartchan@gmail.com>
2018-10-26 22:32:52 +0300

    whitespace in dir name fix

--- lab2/dirs.sh
+++ lab2/dirs.sh
@@ -3,7 +3,8 @@
 declare -a dirs=`find "$1" -maxdepth 1 -type d -print | egrep -v '^[.]{,2}$'`
 declare -a targets
 
-for dir in ${dirs[@]}; do
+for dir in "$1"/*
+do
 	check=`find "$dir" -maxdepth 1 -type d | grep -v "^$dir$"`
 	if [ $? -eq 0 -a -n "$check" ]; then
 		targets+="$dir"$'\n'
