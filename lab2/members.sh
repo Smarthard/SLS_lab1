d389f6841a6684224f009a1554d1cf7f5b1a19be
Smarthard <th3smartchan@gmail.com>
2018-10-26 20:37:41 +0300

    fix: only integer args may pass

--- lab2/members.sh
+++ lab2/members.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-declare -a gids=$(getent passwd | awk -F: '{printf $4 "\n"}' | uniq | sort)
+if [[ $1 =~ ^[+-]?[0-9]+$ ]];
+then
+	declare -a gids=$(getent passwd | awk -F: '{printf $4 "\n"}' | uniq | sort)
 
-for gid in $gids; do
-	count=$(getent passwd | awk -F: '{printf $4 "\n"}' | grep "$gid" | wc -l)
-	if [ $count -gt $1 ]
-	then
-		echo $(getent group "$gid" | cut -d: -f1)
-	fi
-done
+	for gid in $gids; do
+		count=$(getent passwd | awk -F: '{printf $4 "\n"}' | grep "$gid" | wc -l)	
+		if [ $count -gt $1 ]
+		then
+			echo $(getent group "$gid" | cut -d: -f1)
+		fi
+	done
+else
+	echo "Argument must to be an integer number"
+	exit 1
+fi
