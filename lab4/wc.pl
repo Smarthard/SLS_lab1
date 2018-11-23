53a0105fb2679589b1fb2cb1c8aba0e25e75635b
Smarthard <th3smartchan@gmail.com>
2018-11-23 23:20:26 +0300

    options handling

--- lab4/wc.pl
+++ lab4/wc.pl
@@ -3,8 +3,16 @@
 use strict;
 use warnings;
 use utf8;
+use Getopt::Std;
 
+my %opt=();
 my ($tl, $tw, $tc) = (0,0,0);
+
+#########################################################################
+#				MAIN					#
+#########################################################################
+
+getopts("cCmlw", \%opt);
 
 foreach my $fname (@ARGV) {
 	open(FILE, '<'.$fname) or die "Could not open file: $!";
@@ -20,10 +28,18 @@ foreach my $fname (@ARGV) {
 	$tl += $l;
 	$tw += $w;
 	$tc += $c;
-
-	print("$l $w $c $fname\n");
+	
+	print "$c " if $opt{c} or $opt{C} or $opt{m};
+	print "$l " if $opt{l};
+	print "$w " if $opt{w};
+	print "$l $w $c " if %opt == 0;
+	print "$fname\n";
 }
 
 if (scalar(@ARGV) > 1) {
-	print("$tl $tw $tc total\n");
+	print "$tc " if $opt{c} or $opt{C} or $opt{m};
+	print "$tl " if $opt{l};
+	print "$tw " if $opt{w};
+	print "$tl $tw $tc " if %opt == 0;
+	print "total\n";
 }
